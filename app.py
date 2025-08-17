4664dd5c4afc1bbacedadd7aac13682f7e1071be
helmyyoga <helmyyoga@gmail.com>
2025-08-17 21:49:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import asyncio
 import nest_asyncio
 import streamlit as st
 from dotenv import load_dotenv
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 embeddings = GoogleGenerativeAIEmbeddings(api_key=os.environ["GOOGLE_API_KEY"])
 
